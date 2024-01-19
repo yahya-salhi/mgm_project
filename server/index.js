99efc3f36f72aa2d7e04b6f1bb2f0cc8d895100e
yahya-salhi <pcclub10@gmail.com>
2024-01-19 10:09:35 +0100

    Project & Client queries

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const colors = require("colors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
+const app = express();
+//connect to my database mgm_db
+connectDB();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(port, console.log(`server runnig on port ${port}`));
